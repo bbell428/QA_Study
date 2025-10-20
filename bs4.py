79f8d35d11df3770e8aa3b33e2a51163c385e28c
Bbell <428bbell@gmail.com>
2025-10-20 20:00:05 +0900

    BeautifulSoup4
    간단 예제

---
+++ bs4.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://velog.io/@bbell/posts"
+res = requests.get(url)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text, "lxml")
+
+# print(soup.title) # 제목을 가져옴
+# print(soup.title.get_text()) # 텍스트만 가져옴
+
+# print(soup.a) # 문서에서 첫 번째 <a> 태그를 가져옴(soup 객체)
+# print(soup.a.attrs) # a태그 element의 속성을 딕셔너리로 출력
+# print(soup.a["class"]) # a태그 element의 class 속성 값 정보를 출력
+
+# print(soup.find("a", attrs={"class":"VelogTab_item__ijN3R VelogTab_active__P7YGA"}))
+print(soup.find(attrs={"class":"VelogTab_item__ijN3R VelogTab_active__P7YGA"}))
