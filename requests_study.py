d26f21fd6a39af18437b300e1e3a81c0383f2a79
Bbell <428bbell@gmail.com>
2025-10-20 14:36:47 +0900

    requests 예제
    - 원하는 페이지를 접속
    - 정상적으로 정보를 받아오는 지 확인 후
    - html 파일로 만들기

--- requests_study.py
+++ requests_study.py
@@ -1,7 +1,7 @@
 import requests
 res = requests.get("http://google.com")
 # res = requests.get("http://testbbell.tistory.com") # 접근 권한 없음 404
-res.raise_for_status() # 문제가 생겼을 경우 오류를 내뱉고 프로그램 종료
+res.raise_for_status()  # 문제가 생겼을 경우 오류를 내뱉고 프로그램 종료
 print("웹 스크래핑을 진행합니다.")
 
 # print("응답코드 :", res.status_code) # 200 이면 정상
